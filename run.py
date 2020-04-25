87d99be5353e533bdf7444696cc91fb7d8566c1e
Laura Asher <laura.asher@jhuapl.edu>
2020-04-25 14:32:55 -0400

    Make heroku app.

---
+++ run.py
@@ -0,0 +1,13 @@
+
+from flask import Flask, render_template, request
+import json
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+	return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
